c5ae3e6ee3a97604545a9bf9cb1802939b7e08b1
Conor <chepenikconor@gmail.com>
2023-09-13 08:37:48 -0400

    removed unnneccesary stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,22 @@
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-// import { ClerkProvider } from '@clerk/nextjs'
-import { ToasterProvider } from '@/components/toaster-provider'
-import { ModalProvider } from '@/components/modal-provider'
 
-import './globals.css'
-
-const font = Inter({ subsets: ['latin'] });
-const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'MassAdoption.ai',
+  title: 'MassAdoption',
   description: 'Bitcoiner meetup & AI Platform',
-  
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-      <html lang="en" suppressHydrationWarning>
-        <body className={font.className}>
-          <ToasterProvider />
-          <ModalProvider />
-          {children}
-        </body>
-      </html>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   )
 }
