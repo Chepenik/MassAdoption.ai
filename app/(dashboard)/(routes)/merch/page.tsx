71c5332f9551b26c569b7f01f1fd547098fb5a04
Conor <chepenikconor@gmail.com>
2023-09-17 13:03:26 -0400

    updated merch page for mobile friendly viewing

--- app/(dashboard)/(routes)/merch/page.tsx
+++ app/(dashboard)/(routes)/merch/page.tsx
@@ -42,7 +42,7 @@ const MerchPage = () => {
 
   return (
     <div className="container mx-auto mt-10">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {cardData.map((card, index) => (
           <div className="col-span-1" key={index}>
             <div className="bg-black p-4 rounded-lg flex flex-col h-full">
@@ -67,6 +67,5 @@ const MerchPage = () => {
     </div>
   );
 };
-
 
 export default MerchPage;
