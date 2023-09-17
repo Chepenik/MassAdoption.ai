e89db1b145ec70c8ffaceb376d145d1f6a146a47
Conor <chepenikconor@gmail.com>
2023-09-17 13:57:30 -0400

    made it so merch page has 3 cols instead of 4

--- app/(dashboard)/(routes)/merch/page.tsx
+++ app/(dashboard)/(routes)/merch/page.tsx
@@ -44,7 +44,7 @@ const MerchPage = () => {
     <div className="container mx-auto mt-10">
       <h1 className="text-2xl text-center text-white">MassAdoption Merch </h1>
       <br />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-3">
         {cardData.map((card, index) => (
           <div className="col-span-1" key={index}>
             <div className="bg-black p-4 rounded-lg flex flex-col h-full">
