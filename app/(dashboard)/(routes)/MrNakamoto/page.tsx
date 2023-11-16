981189f5924c0a6963a45160013e466f9f9e2bc7
Conor <chepenikconor@gmail.com>
2023-11-16 00:09:43 -0500

    added Mr Nakamoto

--- app/(dashboard)/(routes)/MrNakamoto/page.tsx
+++ app/(dashboard)/(routes)/MrNakamoto/page.tsx
@@ -5,15 +5,17 @@ import Image from "next/image";
 export default function MrNakamotoPage() {
     return (
         <div className="m-4 text-white min-h-screen flex flex-col items-center justify-center text-center py-16">
-            <h1 className="mb-2.5">Introducing Mr. Nakamoto, a Bitcoin LLM designed to answer any question about Bitcoin with accuracy and detail. Mr. Nakamoto is made to deliver pure signal and he will be released soon</h1>
+            <h1 className="mb-2.5">Introducing Mr. Nakamoto, a Bitcoin LLM designed to answer any question about Bitcoin with accuracy and detail. Mr. Nakamoto is made to deliver pure signal ;D</h1>
             <div className="rounded-full overflow-hidden">
-                <Image 
-                    src="/mrnakamoto.jpg"
-                    alt="Mr. Nakamoto"
-                    width={500}
-                    height={500}
-                    layout="responsive"
-                />
+                <a href="https://chat.openai.com/g/g-2Z9CpJRSs-mr-nakamoto" target="_blank" rel="noopener noreferrer">
+                    <Image 
+                        src="/mrnakamoto.jpg"
+                        alt="Mr. Nakamoto"
+                        width={500}
+                        height={500}
+                        layout="responsive"
+                    />
+                </a>
             </div>
         </div>
     );
