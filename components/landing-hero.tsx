6fb1f648480dee76b4d49f9bb30e12ca01ea9392
Conor <chepenikconor@gmail.com>
2023-12-01 09:21:51 -0500

    updated copy on landing page as Jeff wanted

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -8,16 +8,17 @@ import { Button } from "@/components/ui/button";
 export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
-      <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>A Bitcoin Meetup For</h1>
+      <div className="text-xl sm:text-2xl md:text-3xl lg:text-4xl space-y-5 font-extrabold">
+        <h1>MassAdoption Is A Bitcoin Only Company That Concentrates On</h1>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-gray-300 to-orange-500 py-2">
           <TypewriterComponent
             options={{
               strings: [
-                "Anyone willing to learn",
-                "Builders",
-                "Nocoiners",
-                "Maximalists"
+                "Meet Ups",
+                "AI",
+                "Merchant Adoption",
+                "Education",
+                "Freedom Festivals"
               ],
               autoStart: true,
               loop: true,
