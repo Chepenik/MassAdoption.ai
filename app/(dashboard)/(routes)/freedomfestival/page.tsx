8f0d6327b12045e5f446e82b8ac76ff8c6d62101
Conor <chepenikconor@gmail.com>
2023-12-17 16:58:26 -0500

    changes Jeff wanted

--- app/(dashboard)/(routes)/freedomfestival/page.tsx
+++ app/(dashboard)/(routes)/freedomfestival/page.tsx
@@ -1,18 +1,43 @@
 "use client"
 
 import Head from 'next/head';
-import React from 'react';
-import massadoption from "../../../../public/MASS ADOPTIONclearlogo.png"
+import React, { useState, useEffect } from 'react';
+import massadoption from "../../../../public/MASS ADOPTIONclearlogo.png";
 import Link from 'next/link';
 import Image from 'next/image';
+import { FaBars } from 'react-icons/fa';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Home: React.FC = () => {
+  const [isMobileScreen, setIsMobileScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileScreen(window.innerWidth <= 767);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   const speakers = [
+    { name: 'Anders Kargaard', imgSrc: 'https://i.nostr.build/radV.jpg' },
     { name: 'BJ Dichter', imgSrc: 'https://pbs.twimg.com/profile_images/1647631287274840069/vZ8NvccM_400x400.jpg' },
     { name: 'Jason Maier', imgSrc: 'https://pbs.twimg.com/profile_images/1603115142505500677/SB2ferlx_400x400.jpg' },
     { name: 'Texas Slim', imgSrc: 'https://pbs.twimg.com/profile_images/1589784780970795011/02jf0fqp_400x400.jpg' },
-    { name: 'Speaker 4 coming soon', imgSrc: 'https://cdn.nostr.build/p/nb1656.jpeg' },
-    { name: 'Speaker 5 coming soon', imgSrc: 'https://i.nostr.build/a6kq.png' },
+    { name: 'David Foley', imgSrc: 'https://i.nostr.build/ZLY8.jpg' },
     { name: 'Speaker 6 coming soon', imgSrc: 'https://i.nostr.build/eAJG.png' },
     { name: 'Speaker 7 coming soon', imgSrc: 'https://cdn.nostr.build/p/nb1655.jpg' },
     { name: 'Speaker 8 coming soon', imgSrc: 'https://i.nostr.build/O8EQ.png' },
@@ -27,20 +52,35 @@ const Home: React.FC = () => {
         <link rel="icon" href="/bitcoin-favicon.ico" />
       </Head>
 
-      <nav className="flex justify-between items-center p-4 shadow-md bg-slate-700">
-      <Link href="/" passHref>
-        <div className="flex items-center cursor-pointer">
-          <Image alt="Logo" src={massadoption} className="rounded-full" width={96} height={96} />
-        </div>
-      </Link>
-      <div>
-        <Link href="#about" passHref><span className="p-2 hover:underline text-white cursor-pointer">About</span></Link>
-        <Link href="#lineup" passHref><span className="p-2 hover:underline text-white cursor-pointer">Lineup</span></Link>
-        <Link href="#activities" passHref><span className="p-2 hover:underline text-white cursor-pointer">Activities</span></Link>
-        <Link href="https://massadoptionbtc.ticketspice.com/massadoption-2024" passHref><span className="p-2 hover:underline text-white cursor-pointer">Tickets</span></Link>
-        <Link href="#faq" passHref><span className="p-2 hover:underline text-white cursor-pointer">FAQ</span></Link>
-      </div>
-    </nav>
+      <nav className="p-4 bg-transparent text-white flex items-center justify-between">
+        <Link href="/" passHref>
+          <div className="relative h-24 w-24 mr-6">
+            <Image alt="Logo" src={massadoption} className="rounded-full" />
+          </div>
+        </Link>
+        {isMobileScreen ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <FaBars className="text-xl" />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <Link href="#about" passHref><DropdownMenuItem>About</DropdownMenuItem></Link>
+              <Link href="#lineup" passHref><DropdownMenuItem>Lineup</DropdownMenuItem></Link>
+              <Link href="#activities" passHref><DropdownMenuItem>Activities</DropdownMenuItem></Link>
+              <Link href="https://massadoptionbtc.ticketspice.com/massadoption-2024" passHref><DropdownMenuItem>Tickets</DropdownMenuItem></Link>
+              <Link href="#faq" passHref><DropdownMenuItem>FAQ</DropdownMenuItem></Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : (
+          <div className="hidden lg:flex items-center gap-x-2">
+            <Link href="#about" passHref><span className="text-lg font-semibold hover:text-orange-400 hover:underline cursor-pointer">About</span></Link>
+            <Link href="#lineup" passHref><span className="text-lg font-semibold hover:text-orange-400 hover:underline cursor-pointer">Lineup</span></Link>
+            <Link href="#activities" passHref><span className="text-lg font-semibold hover:text-orange-400 hover:underline cursor-pointer">Activities</span></Link>
+            <Link href="https://massadoptionbtc.ticketspice.com/massadoption-2024" passHref><span className="text-lg font-semibold hover:text-orange-400 hover:underline cursor-pointer">Tickets</span></Link>
+            <Link href="#faq" passHref><span className="text-lg font-semibold hover:text-orange-400 hover:underline cursor-pointer">FAQ</span></Link>
+          </div>
+        )}
+      </nav>
 
       <main className="p-8 bg-gray-900">
         <h1 className="text-4xl font-bold mb-6 text-white">Embrace the Future of Finance and Freedom</h1>
@@ -50,7 +90,9 @@ const Home: React.FC = () => {
         
         <section id="about" className="my-8">
           <h2 className="text-3xl font-bold mb-4 text-white">About the Festival</h2>
-          <p className="text-gray-300">Freedom Festival 2024 is not just an event; it's a movement. With a focus on the transformative power of Bitcoin, we're gathering freedom-loving individuals to discuss, debate, and celebrate the role of Bitcoin in securing personal liberty.</p>
+          <p className="text-gray-300">Freedom Festival 2024 is not just an event; it's a movement. With a focus on living a bitcoin lifestyle with the transformative power of Bitcoin. We are gathering freedom-loving individuals to discuss, debate, and celebrate the role of Bitcoin in securing personal liberty and sovereignty.
+
+Buy your tickets today and rub elbows with your favorite bitcoiners, have vigorous debate about bitcoin/fiat, participate in bitcoin’s value for value circular economy, bitcoin education and most importantly, the freedom that bitcoin provides to be a free and sovereign individual.</p>
         </section>
         
         <section id="lineup" className="my-8">
@@ -72,9 +114,10 @@ const Home: React.FC = () => {
               <li>Parkour Demonstrations</li>
               <li>Interactive Workshops</li>
               <li>Cattlemen’s Feast</li>
-              <li>Soccer Tournaments</li>
-              <li>VC Networking Hour</li>
-              {/* Add more activities here */}
+              <li>Live music</li>
+              <li>Bitcoin DJ’s</li>
+              <li>Soccer tournament</li>
+              <li>Contests/prizes</li>
             </ul>
           </div>
         </section>
--- components/FestivalBanner.tsx
+++ components/FestivalBanner.tsx
@@ -1,17 +1,27 @@
 const FestivalBanner = () => {
-    return (
-      <div className="festival-banner bg-orange-600 text-white text-center py-4">
-        <h2 className="text-2xl font-bold">Join us for the Mass Adoption 2024 Festival!</h2>
+  return (
+    <div className="festival-banner bg-orange-600 text-white text-center py-4">
+      <h2 className="text-2xl font-bold">Join us for the Mass Adoption 2024 Festival!</h2>
+      <div className="inline-flex justify-center mt-2 gap-4">
         <a
           href="https://massadoptionbtc.ticketspice.com/massadoption-2024"
           target="_blank"
           rel="noopener noreferrer"
-          className="mt-2 inline-block bg-blue-800 text-white py-2 px-4 rounded hover:bg-purple-500 transition duration-300"
+          className="bg-blue-800 text-white py-2 px-4 rounded hover:bg-purple-500 transition duration-300"
         >
           Get Tickets
         </a>
+        <a
+          href="/freedomfestival"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500 transition duration-300"
+        >
+          Learn More
+        </a>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-export default FestivalBanner
+export default FestivalBanner;
