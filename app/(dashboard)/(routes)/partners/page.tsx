056f4730252fcf63904dc7d087537eaa2c4624fb
Conor <chepenikconor@gmail.com>
2024-01-10 23:11:32 -0500

    added bitcoin news

--- app/(dashboard)/(routes)/partners/page.tsx
+++ app/(dashboard)/(routes)/partners/page.tsx
@@ -30,6 +30,11 @@ const partners = [
     image: "https://i.nostr.build/y92m.webp"
   },
   { 
+    name: "BitcoinNews.com",
+    url: "https://bitcoinnews.com/",
+    image: "https://i.nostr.build/D9xV.png"
+  },
+  { 
     name: "Satoshi's Journal", 
     url: "https://satoshisjournal.com/",
     image: "https://i.nostr.build/X7nl.png" 
@@ -48,7 +53,7 @@ const Partners: React.FC = () => {
         MassAdoption is proud partners with:
       </h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
         {partners.map((partner, index) => (
           <Link href={partner.url} key={index}>
           <div className="border rounded-md p-4 hover:bg-gray-100">
