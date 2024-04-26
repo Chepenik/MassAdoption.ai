653768d57a7e2970073ba952e5f72fe739d7faae
Conor <chepenikconor@gmail.com>
2024-04-26 13:14:39 -0400

    added one week left banner

--- components/FestivalBanner.tsx
+++ components/FestivalBanner.tsx
@@ -2,6 +2,7 @@ const FestivalBanner = () => {
   return (
     <div className="festival-banner bg-orange-600 text-white text-center py-4">
       <h2 className="text-2xl font-bold">Join us for the Mass Adoption 2024 Freedom Festival!</h2>
+      <h3 className="text-xl font-bold mt-2 text-purple-300">Less Than 1 Week Left. Buy Tickets Now</h3>
       <div className="inline-flex justify-center mt-2 gap-4">
         <a
           href="https://massadoptionbtc.ticketspice.com/massadoption-2024"
