9a0432568be3272d603fc875a06aff03369c1867
Conor <chepenikconor@gmail.com>
2024-05-05 14:33:51 -0400

    removed Freedom Festival stuff

--- components/FestivalBanner.tsx
+++ components/FestivalBanner.tsx
@@ -1,28 +1,28 @@
-const FestivalBanner = () => {
-  return (
-    <div className="festival-banner bg-orange-600 text-white text-center py-4">
-      <h2 className="text-2xl font-bold">Join us for the Mass Adoption 2024 Freedom Festival!</h2>
-      <h3 className="text-xl font-bold mt-2 text-purple-300">Less Than 1 Week Left. Buy Tickets Now</h3>
-      <div className="inline-flex justify-center mt-2 gap-4">
-        <a
-          href="https://massadoptionbtc.ticketspice.com/massadoption-2024"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-blue-800 text-white py-2 px-4 rounded hover:bg-purple-500 transition duration-300"
-        >
-          Get Tickets
-        </a>
-        <a
-          href="/freedomfestival"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500 transition duration-300"
-        >
-          Learn More
-        </a>
-      </div>
-    </div>
-  );
-};
+// const FestivalBanner = () => {
+//   return (
+//     <div className="festival-banner bg-orange-600 text-white text-center py-4">
+//       <h2 className="text-2xl font-bold">Join us for the Mass Adoption 2024 Freedom Festival!</h2>
+//       <h3 className="text-xl font-bold mt-2 text-purple-300">Less Than 1 Week Left. Buy Tickets Now</h3>
+//       <div className="inline-flex justify-center mt-2 gap-4">
+//         <a
+//           href="https://massadoptionbtc.ticketspice.com/massadoption-2024"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="bg-blue-800 text-white py-2 px-4 rounded hover:bg-purple-500 transition duration-300"
+//         >
+//           Get Tickets
+//         </a>
+//         <a
+//           href="/freedomfestival"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//           className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-500 transition duration-300"
+//         >
+//           Learn More
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default FestivalBanner;
+// export default FestivalBanner;
