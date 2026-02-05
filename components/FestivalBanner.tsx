2acba2e5a32ce914cc107000a9eb20c26f9587df
Binmucker Chep <chepenikconor@gmail.com>
2026-02-04 19:53:20 -0500

    update the banner

--- components/FestivalBanner.tsx
+++ components/FestivalBanner.tsx
@@ -6,7 +6,7 @@ const FestivalBanner = () => {
       <h2 className="text-3xl font-bold">Join us at the first ever island Bitcoin retreat</h2>
       <h2 className="text-3xl font-bold">CAMP NAKAMOTO 2026</h2>
       <div className="inline-flex justify-center mt-4 gap-6">
-        <Link href="https://massadoptionbtc.ticketspice.com/camp-nakamoto" legacyBehavior>
+        <Link href="https://pay.zaprite.com/pl_nbISw4YWoJ" legacyBehavior>
           <a target="_blank" rel="noopener noreferrer" className="bg-red-600 text-white py-3 px-6 rounded-lg hover:bg-yellow-500 transition duration-300 ease-in-out">
             Get Tickets
           </a>
